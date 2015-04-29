60205e112a21341db0e87031970d4f812807a8ab
dignifiedquire <dignifiedquire@gmail.com>
2015-04-29 16:38:59 +0200

    Add compiled methods and package.json

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ES 6 Talk - 29.04.2015
+
+```bash
+# Run
+$ npm run all
+# Compile
+$ npm run compile
+```
---
+++ index.js
@@ -0,0 +1,4 @@
+
+import mistakes from './src/mistakes'
+import sugar from './src/sugar'
+import libraries from './src/libraries'
